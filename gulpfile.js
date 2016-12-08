791f1f939f3f46501eccfa53f2aaefa6234ccecb
Orlando Masso <orlandomassoj@hotmail.com>
2016-12-08 17:22:24 -0500

    Template Finalized

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,16 @@ var sourcemaps = require('gulp-sourcemaps');
 // Compile SASS
 
 gulp.task('sass', function() {
-    return gulp.src('./app/sass/*.sass', './app/sass/pages/*.sass') // Create a stream in the directory where our Sass files are located.
+    return gulp.src('./BlyndSide/app/sass/*.sass') // Create a stream in the directory where our Sass files are located.
     .pipe(sourcemaps.init())        //init sourcemap
     .pipe(sass())                    // Compile Sass into style.css.
-    .pipe(autoprefixer({            //autoprefix
+     .pipe(autoprefixer({            //autoprefix
       browsers: ['last 2 versions'],
       cascade: false
     }))
     .pipe(cssnano())                //minify css
     .pipe(sourcemaps.write('./'))     //create sourcemap
-    .pipe(gulp.dest('./app/sass/style.css'));      // Write style.css to the project's root directory.
+    .pipe( gulp.dest('./style.css') );      // Write style.css to the project's root directory.
 
 });
 
@@ -35,8 +35,10 @@ gulp.task('watch', function() {
 
   });
 
-  gulp.watch('./app/sass/pages/*.sass', ['sass']).on('change', browserSync.reload);  // If a file changes, re-run 'sass'
-  gulp.watch('./app/sass/*.sass', ['sass']).on('change', browserSync.reload);
+    // If a file changes, re-run 'sass'
+  gulp.watch('./app/sass/*.sass', ['sass'], function() {
+    browserSync.reload();
+  });
   gulp.watch('./app/index.html', function() {
     browserSync.reload();
   });
