c538d166533d5aa7cdd297b4f08a9ef8b5937e6d
Orlando Masso <orlandomassoj@hotmail.com>
2017-01-26 01:28:00 -0500

    added pages
    js animations, resizing, unstable mobile

--- app/js/functions.js
+++ app/js/functions.js
@@ -1,126 +1,143 @@
 $(document).ready(function() {
-    $('#pagepiling').pagepiling({
-        menu: null,
-        direction: 'vertical',
-        verticalCentered: false,
-        sectionsColor: ['#2e2e2f', 'white', '#2e2e2f', 'white', '#2e2e2f', 'white', '#7d45a8'],
-        anchors: ['Home', 'Design', 'Customers', 'How', 'Pricing', 'Pros', 'Contact'],
-        scrollingSpeed: 700,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['Home', 'Design', 'Customers', 'How It Works', 'Pricing', 'Pros Services', 'Contact']
-        },
-        normalScrollElements: null,
-        normalScrollElementTouchThreshold: 25,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
 
-        //events
 
-        //afterLoad: function(anchorLink, index){
+        //}
 
-        onLeave: function(index, nextIndex, direction) {
-        //Design Animation
-        if(index == 1, nextIndex == 2, direction == 'down'){
-            $("#dominion").addClass('animated fadeInLeft');
-        }else{
-            $("#dominion").removeClass('animated fadeInLeft');
-        }
-        if(index == 1, nextIndex == 2, direction == 'down'){
-            $("#row-web").addClass('animated fadeInUp');
-        }else{
-            $("#row-web").removeClass('animated fadeInUp');
-        }
-        if(index == 1, nextIndex == 2, direction == 'down'){
-            $("#start-btn").addClass('animated fadeInUp');
-        }else{
-            $("#start-btn").removeClass('animated fadeInUp');
-        }
-        //How it works animations
-        if(index == 3, nextIndex == 4, direction == 'down'){
-            $("#headline-how").addClass('animated fadeInUp');
-        }else{
-            $("#headline-how").removeClass('animated fadeInUp');
-        }
-        if(index == 3, nextIndex == 4, direction == 'down'){
-            $("#how-mac").addClass('animated fadeInUp');
-        }else{
-            $("#how-mac").removeClass('animated fadeInUp');
-        }
-        if(index == 3, nextIndex == 4, direction == 'down'){
-            $("#blurb").addClass('animated fadeInUp');
-        }else{
-            $("#blurb").removeClass('animated fadeInUp');
-        }
-        if(index == 3, nextIndex == 4, direction == 'down'){
-            $("#blurb2").addClass('animated fadeInUp');
-        }else{
-            $("#blurb2").removeClass('animated fadeInUp');
-        }
-        if(index == 3, nextIndex == 4, direction == 'down'){
-            $("#blurb3").addClass('animated fadeInUp');
-        }else{
-            $("#blurb3").removeClass('animated fadeInUp');
-        }
-        //Pricing animations
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $("#headline-pricing").addClass('animated fadeInUp');
-        }else{
-            $("#headline-pricing").removeClass('animated fadeInUp');
-        }
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $("#text-pricing").addClass('animated fadeInUp');
-        }else{
-            $("#text-pricing").removeClass('animated fadeInUp');
-        }
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $(".border-bronze").addClass('animated fadeInUp');
-        }else{
-            $(".border-bronze").removeClass('animated fadeInUp');
-        }
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $(".border-silver").addClass('animated fadeInUp');
-        }else{
-            $(".border-silver").removeClass('animated fadeInUp');
-        }
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $(".border-gold").addClass('animated fadeInUp');
-        }else{
-            $(".border-gold").removeClass('animated fadeInUp');
-        }
-        if(index == 4, nextIndex == 5, direction == 'down'){
-            $(".border-platinum").addClass('animated fadeInUp');
-        }else{
-            $(".border-platinum").removeClass('animated fadeInUp');
-        }
-        //customers animations
-        if(index == 2, nextIndex == 3, direction == 'down'){
-            $("#headline-customers").addClass('animated fadeInUp');
-        }else{
-            $("#headline-customers").removeClass('animated fadeInUp');
-        }
-        if(index == 2, nextIndex == 3, direction == 'down'){
-            $("#customers-container").addClass('animated fadeInUp');
-        }else{
-            $("#customers-container").removeClass('animated fadeInUp');
-        }
-      }
+          //onLeave: function(index, nextIndex, direction) {
+          //How it works animations
 
-        //afterRender: function() {},
-    });
+        //}
+      //});
+
+//afterRender: function() {},
+
     setTimeout(function() {
         $('body').addClass('loaded');
         $('h1').css('color', '#222222');
     }, 3000);
+});
+
+$(window).scroll(function() {
+    if ($(this).width() < 2000) {
+        if ($(this).height() <= 960) {
+            if ($(this).scrollTop() < 500) {
+            $('#dominion').hide();
+            $('#row-content').hide();
+            $('#start-btn').hide();
+
+            }
+            if($(this).scrollTop() > 600) {
+
+            //web animations
+            $('#dominion').show();
+            $('#row-content').show();
+            $('#start-btn').show();
+
+            $('#dominion').addClass('animated fadeInLeft');
+            $('#row-content').addClass('animated fadeInUp');
+            $('#start-btn').addClass('animated fadeInUp');
+
+
+            }
+            //how it works animations
+            if($(this).scrollTop() < 1800) {
+
+            $('#headline-how').hide();
+            $('#how-mac').hide();
+            $('#blurb').hide();
+            $('#blurb2').hide();
+            $('#blurb3').hide();
+
+            }
+            if($(this).scrollTop() > 1800) {
+
+            $('#headline-how').show();
+            $('#how-mac').show();
+            $('#blurb').show();
+            $('#blurb2').show();
+            $('#blurb3').show();
+
+            $('#headline-how').addClass('animated fadeInUp');
+            $('#how-mac').addClass('animated fadeInUp');
+            $('#blurb').addClass('animated fadeInUp');
+            $('#blurb2').addClass('animated fadeInUp');
+            $('#blurb3').addClass('animated fadeInUp');
+
+            }
+            //pricing animations
+
+            if($(this).scrollTop() < 2300) {
+
+            $('#headline-pricing').hide();
+            $('#text-pricing').hide();
+            $('.border-bronze').hide();
+            $('.border-silver').hide();
+            $('.border-gold').hide();
+            $('.border-platinum').hide();
+
+            }
+            if($(this).scrollTop() > 2400) {
+
+            $('#headline-pricing').show();
+            $('#text-pricing').show();
+            $('.border-bronze').show();
+            $('.border-silver').show();
+            $('.border-gold').show();
+            $('.border-platinum').show();
+
+            $('#headline-pricing').addClass('animated fadeInUp');
+            $('#text-pricing').addClass('animated fadeInUp');
+            $('.border-bronze').addClass('animated bounceIn');
+            $('.border-silver').addClass('animated bounceIn');
+            $('.border-gold').addClass('animated bounceIn');
+            $('.border-platinum').addClass('animated bounceIn');
+            }
+
+            //pro services animations
+            if ($(this).scrollTop() < 3400) {
+            $('.headline-services').hide();
+            $('#services-container').hide();
+
+
+            }
+            if($(this).scrollTop() > 3750) {
+
+            //web animations
+            $('.headline-services').show();
+            $('#services-container').show();
+
+
+            $('.headline-services').addClass('animated fadeInUp');
+            $('#services-container').addClass('animated zoomIn');
 
 
 
+            }
+            //contact animations
+            if($(this).scrollTop() < 4700) {
+
+            $('#headline-contact').hide();
+            $('#contact1').hide();
+            $('#contact2').hide();
+            $('#contact3').hide();
+            $('#contact4').hide();
+
+            }
+            if($(this).scrollTop() > 4800) {
+
+            $('#headline-contact').show();
+            $('#contact1').show();
+            $('#contact2').show();
+            $('#contact3').show();
+            $('#contact4').show();
+
+            $('#headline-contact').addClass('animated fadeInUp');
+            $('#contact1').addClass('animated bounceIn');
+            $('#contact2').addClass('animated bounceIn');
+            $('#contact3').addClass('animated bounceIn');
+            $('#contact4').addClass('animated bounceIn');
+
+            }
+        }
+    }
 });
--- app/js/min/functions-min.js
+++ app/js/min/functions-min.js
@@ -1,1 +1,145 @@
-$(document).ready(function(){$("#pagepiling").pagepiling({menu:null,direction:"vertical",verticalCentered:!1,sectionsColor:["#2e2e2f","white","#2e2e2f","white","#2e2e2f","white","#7d45a8"],anchors:["Home","Design","Customers","How","Pricing","Pros","Contact"],scrollingSpeed:700,easing:"swing",loopBottom:!1,loopTop:!1,css3:!0,navigation:{textColor:"#000",bulletsColor:"#000",position:"right",tooltips:["Home","Design","Customers","How It Works","Pricing","Pros Services","Contact"]},normalScrollElements:null,normalScrollElementTouchThreshold:25,touchSensitivity:5,keyboardScrolling:!0,sectionSelector:".section",animateAnchor:!1,onLeave:function(e,a,d){"down"==d?$("#dominion").addClass("animated fadeInLeft"):$("#dominion").removeClass("animated fadeInLeft"),"down"==d?$("#row-web").addClass("animated fadeInUp"):$("#row-web").removeClass("animated fadeInUp"),"down"==d?$("#start-btn").addClass("animated fadeInUp"):$("#start-btn").removeClass("animated fadeInUp"),"down"==d?$("#headline-how").addClass("animated fadeInUp"):$("#headline-how").removeClass("animated fadeInUp"),"down"==d?$("#how-mac").addClass("animated fadeInUp"):$("#how-mac").removeClass("animated fadeInUp"),"down"==d?$("#blurb").addClass("animated fadeInUp"):$("#blurb").removeClass("animated fadeInUp"),"down"==d?$("#blurb2").addClass("animated fadeInUp"):$("#blurb2").removeClass("animated fadeInUp"),"down"==d?$("#blurb3").addClass("animated fadeInUp"):$("#blurb3").removeClass("animated fadeInUp"),"down"==d?$("#headline-pricing").addClass("animated fadeInUp"):$("#headline-pricing").removeClass("animated fadeInUp"),"down"==d?$("#text-pricing").addClass("animated fadeInUp"):$("#text-pricing").removeClass("animated fadeInUp"),"down"==d?$(".border-bronze").addClass("animated fadeInUp"):$(".border-bronze").removeClass("animated fadeInUp"),"down"==d?$(".border-silver").addClass("animated fadeInUp"):$(".border-silver").removeClass("animated fadeInUp"),"down"==d?$(".border-gold").addClass("animated fadeInUp"):$(".border-gold").removeClass("animated fadeInUp"),"down"==d?$(".border-platinum").addClass("animated fadeInUp"):$(".border-platinum").removeClass("animated fadeInUp"),"down"==d?$("#headline-customers").addClass("animated fadeInUp"):$("#headline-customers").removeClass("animated fadeInUp"),"down"==d?$("#customers-container").addClass("animated fadeInUp"):$("#customers-container").removeClass("animated fadeInUp")}}),setTimeout(function(){$("body").addClass("loaded"),$("h1").css("color","#222222")},3e3)});
+$(document).ready(function() {
+
+
+        //}
+
+          //onLeave: function(index, nextIndex, direction) {
+          //How it works animations
+
+        //}
+      //});
+
+//afterRender: function() {},
+
+    setTimeout(function() {
+        $('body').addClass('loaded');
+        $('h1').css('color', '#222222');
+    }, 3000);
+});
+
+$(window).scroll(function() {
+    if ($(this).width() < 2000) {
+        if ($(this).height() <= 960) {
+            if ($(this).scrollTop() < 500) {
+            $('#dominion').hide();
+            $('#row-content').hide();
+            $('#start-btn').hide();
+
+            }
+            if($(this).scrollTop() > 600) {
+
+            //web animations
+            $('#dominion').show();
+            $('#row-content').show();
+            $('#start-btn').show();
+
+            $('#dominion').addClass('animated fadeInLeft');
+            $('#row-content').addClass('animated fadeInUp');
+            $('#start-btn').addClass('animated fadeInUp');
+
+
+            }
+            //how it works animations
+            if($(this).scrollTop() < 1800) {
+
+            $('#headline-how').hide();
+            $('#how-mac').hide();
+            $('#blurb').hide();
+            $('#blurb2').hide();
+            $('#blurb3').hide();
+
+            }
+            if($(this).scrollTop() > 1800) {
+
+            $('#headline-how').show();
+            $('#how-mac').show();
+            $('#blurb').show();
+            $('#blurb2').show();
+            $('#blurb3').show();
+
+            $('#headline-how').addClass('animated fadeInUp');
+            $('#how-mac').addClass('animated fadeInUp');
+            $('#blurb').addClass('animated fadeInUp');
+            $('#blurb2').addClass('animated fadeInUp');
+            $('#blurb3').addClass('animated fadeInUp');
+
+            }
+            //pricing animations
+
+            if($(this).scrollTop() < 2300) {
+
+            $('#headline-pricing').hide();
+            $('#text-pricing').hide();
+            $('.border-bronze').hide();
+            $('.border-silver').hide();
+            $('.border-gold').hide();
+            $('.border-platinum').hide();
+
+            }
+            if($(this).scrollTop() > 2400) {
+
+            $('#headline-pricing').show();
+            $('#text-pricing').show();
+            $('.border-bronze').show();
+            $('.border-silver').show();
+            $('.border-gold').show();
+            $('.border-platinum').show();
+
+            $('#headline-pricing').addClass('animated fadeInUp');
+            $('#text-pricing').addClass('animated fadeInUp');
+            $('.border-bronze').addClass('animated bounceIn');
+            $('.border-silver').addClass('animated bounceIn');
+            $('.border-gold').addClass('animated bounceIn');
+            $('.border-platinum').addClass('animated bounceIn');
+            }
+
+            //pro services animations
+            if ($(this).scrollTop() < 3400) {
+            $('.headline-services').hide();
+            $('#services-container').hide();
+
+
+            }
+            if($(this).scrollTop() > 3750) {
+
+            //web animations
+            $('.headline-services').show();
+            $('#services-container').show();
+
+
+            $('.headline-services').addClass('animated fadeInUp');
+            $('#services-container').addClass('animated zoomIn');
+
+
+
+            }
+            //contact animations
+            if($(this).scrollTop() < 4700) {
+
+            $('#headline-contact').hide();
+            $('#contact1').hide();
+            $('#contact2').hide();
+            $('#contact3').hide();
+            $('#contact4').hide();
+
+            }
+            if($(this).scrollTop() > 4800) {
+
+            $('#headline-contact').show();
+            $('#contact1').show();
+            $('#contact2').show();
+            $('#contact3').show();
+            $('#contact4').show();
+
+            $('#headline-contact').addClass('animated fadeInUp');
+            $('#contact1').addClass('animated bounceIn');
+            $('#contact2').addClass('animated bounceIn');
+            $('#contact3').addClass('animated bounceIn');
+            $('#contact4').addClass('animated bounceIn');
+
+            }
+        }
+    }
+});
+
+
